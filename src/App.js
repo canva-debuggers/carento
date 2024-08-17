b66ed405b8482be10678051525a2a8b779ad6a4b
Manashi Roy <manashi.roy@icloud.com>
2024-08-18 02:56:15 +0530

    log in page integration

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import DefaultLayout from "./layout/DefaultLayout";
+import AccountProvider from "./context/AccountProvider";
+import PrivateRouter from "./components/PrivateRouter";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AccountProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route path="/dashboard" element={<PrivateRouter><Dashboard /></PrivateRouter>} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AccountProvider>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {
   Button,
   Col,
@@ -9,55 +9,199 @@ import {
   Row,
 } from "react-bootstrap";
 import logoCarImg from "../assets/login_car.jpg";
+import { app } from "../firebase";
+import { AccountContext } from "../context/AccountProvider";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import {
+  createUser,
+  getAllDataFromCollection,
+  googleUser,
+  loginUser,
+  retrieveAllDataFromCollection,
+  storeJsonInCollection,
+} from "../queries/queries";
 
 function Login() {
+  const { setAccount } = useContext(AccountContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [loginForm, setLoginForm] = useState(1);
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    const user = createUser(email, password);
+    console.log("user", user);
+    setAccount(user);
+    navigate("/dashboard");
+  };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const user = loginUser(email, password);
+
+    setAccount(user);
+    navigate("/dashboard");
+  };
+  const loginWithGoogle = async () => {
+    try {
+      const user = await googleUser();
+      setAccount(user);
+      navigate("/dashboard");
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <Container fluid style={{ backgroundColor: "#000", minHeight: "100vh" }}>
       <Row>
         <Col xs={12}>
           <div>
-            <h3 style={{ color: "#fff", fontSize: "2rem" }}>Carento.</h3>
-            <h1
-              className="text-center"
-              style={{ color: "#fff", fontSize: "3rem" }}
-            >
+            <div className="fs-3 text-white opacity-50">Carento.</div>
+            <div className="fs-1" style={{ color: "#fff" }}>
               Premium Cars. Enjoy the Luxury.
-            </h1>
-            <Form>
-              <FloatingLabel
-                controlId="floatingInput"
-                label="Email address"
-                className="mb-3"
-              >
-                <Form.Control
-                  type="email"
-                  placeholder="name@example.com"
+            </div>
+            {loginForm === 1 && (
+              <Form onSubmit={handleLogin}>
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Email address"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    type="email"
+                    placeholder="name@example.com"
+                    value={email}
+                    style={{
+                      width: "20rem",
+                    }}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FloatingLabel>
+
+                <FloatingLabel controlId="floatingPassword" label="Password">
+                  <Form.Control
+                    type="password"
+                    style={{ width: "20rem" }}
+                    value={password}
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FloatingLabel>
+
+                <Button
+                  type="submit"
                   style={{
                     width: "20rem",
-                    height: "2rem",
-                    borderRadius: "9px",
-                    padding: "0.2rem 0.3rem",
+                    borderRadius: "40px",
+                    height: "3rem",
+                    fontWeight: "bold",
+                    background: "#fff",
+                    marginTop: "1rem",
                   }}
-                />
-              </FloatingLabel>
+                >
+                  <span style={{ color: "#000" }} className="fs-6">
+                    Let's Go
+                  </span>
+                </Button>
+                <div
+                  style={{ color: "#fff" }}
+                  className="fs-6 text-center"
+                  onClick={() => setLoginForm(2)}
+                >
+                  New user? Register Here
+                </div>
+              </Form>
+            )}
+            {loginForm === 2 && (
+              <Form onSubmit={handleRegister}>
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Name"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    type="text"
+                    placeholder=""
+                    value={name}
+                    style={{
+                      width: "20rem",
+                    }}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </FloatingLabel>
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Email address"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    type="email"
+                    placeholder="name@example.com"
+                    value={email}
+                    style={{
+                      width: "20rem",
+                    }}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FloatingLabel>
 
-              <FloatingLabel controlId="floatingPassword" label="Password">
-                <Form.Control type="password" placeholder="Password" />
-              </FloatingLabel>
+                <FloatingLabel controlId="floatingPassword" label="Password">
+                  <Form.Control
+                    type="password"
+                    style={{ width: "20rem" }}
+                    value={password}
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FloatingLabel>
 
-              <Button
-                variant="primary"
-                type="submit"
-                style={{
-                  width: "35rem",
-                  borderRadius: "40px",
-                  height: "3rem",
-                  fontWeight: "bold",
-                }}
-              >
-                Let's Go
-              </Button>
-            </Form>
+                <Button
+                  type="submit"
+                  style={{
+                    width: "20rem",
+                    borderRadius: "40px",
+                    height: "3rem",
+                    fontWeight: "bold",
+                    background: "#fff",
+                    marginTop: "1rem",
+                  }}
+                >
+                  <span style={{ color: "#000" }} className="fs-6">
+                    Let's Go
+                  </span>
+                </Button>
+                <Button
+                  type="button"
+                  style={{
+                    width: "20rem",
+                    borderRadius: "40px",
+                    height: "3rem",
+                    fontWeight: "bold",
+                    background: "#fff",
+                    marginTop: "1rem",
+                  }}
+                >
+                  <span
+                    style={{ color: "#000" }}
+                    className="fs-6"
+                    onClick={loginWithGoogle}
+                  >
+                    Log in with google
+                  </span>
+                </Button>
+                <div
+                  style={{ color: "#fff" }}
+                  className="fs-6 text-center"
+                  onClick={() => setLoginForm(1)}
+                >
+                  Already an user? Login Here
+                </div>
+              </Form>
+            )}
           </div>
         </Col>
         <Col xs={12}>
